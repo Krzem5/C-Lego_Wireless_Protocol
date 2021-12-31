7556281590f0cd469a1540277c1625fa8961220d
Krzem <krzem5.dev@gmail.com>
2021-12-31 12:16:39 +0100

    Push Update 31/12/2021, 12:16:39

--- build.py
+++ build.py
@@ -18,14 +18,9 @@ else:
 	os.mkdir("build")
 cd=os.getcwd()
 os.chdir("build")
-if ("--release" in sys.argv):
-	if (subprocess.run(["cl","/c","/permissive-","/GS","/utf-8","/W3","/Zc:wchar_t","/Gm-","/sdl","/Zc:inline","/fp:precise","/D","NDEBUG","/D","_WINDOWS","/D","_UNICODE","/D","UNICODE","/errorReport:none","/WX","/Zc:forScope","/Gd","/Oi","/FC","/EHsc","/nologo","/diagnostics:column","/GL","/Gy","/Zi","/O2","/Oi","/MD","/I","../src/include","../src/main.c","../src/lego_wireless_protocol/*.c"]).returncode!=0 or subprocess.run(["link","*.obj","/OUT:lego_wireless_protocol.exe","/DYNAMICBASE","kernel32.lib","user32.lib","gdi32.lib","winspool.lib","comdlg32.lib","advapi32.lib","shell32.lib","ole32.lib","oleaut32.lib","uuid.lib","odbc32.lib","odbccp32.lib","/MACHINE:X64","/SUBSYSTEM:CONSOLE","/ERRORREPORT:none","/NOLOGO","/TLBID:1","/WX","/LTCG","/OPT:REF","/INCREMENTAL:NO","/OPT:ICF"]).returncode!=0):
-		os.chdir(cd)
-		sys.exit(1)
-else:
-	if (subprocess.run(["cl","/c","/permissive-","/GS","/utf-8","/W3","/Zc:wchar_t","/Gm-","/sdl","/Zc:inline","/fp:precise","/D","_DEBUG","/D","_WINDOWS","/D","_UNICODE","/D","UNICODE","/errorReport:none","/WX","/Zc:forScope","/Gd","/Oi","/FC","/EHsc","/nologo","/diagnostics:column","/ZI","/Od","/RTC1","/MDd","/I","../src/include","../src/main.c","../src/lego_wireless_protocol/*.c"]).returncode!=0 or subprocess.run(["link","*.obj","/OUT:lego_wireless_protocol.exe","/DYNAMICBASE","kernel32.lib","user32.lib","gdi32.lib","winspool.lib","comdlg32.lib","advapi32.lib","shell32.lib","ole32.lib","oleaut32.lib","uuid.lib","odbc32.lib","odbccp32.lib","/MACHINE:X64","/SUBSYSTEM:CONSOLE","/ERRORREPORT:none","/NOLOGO","/TLBID:1","/WX","/DEBUG","/INCREMENTAL"]).returncode!=0):
-		os.chdir(cd)
-		sys.exit(1)
+if (subprocess.run(["cl","/c","/permissive-","/GS","/utf-8","/W3","/Zc:wchar_t","/Gm-","/sdl","/Zc:inline","/fp:precise","/D","_DEBUG","/D","_WINDOWS","/D","_UNICODE","/D","UNICODE","/errorReport:none","/WX","/Zc:forScope","/Gd","/Oi","/FC","/EHsc","/nologo","/diagnostics:column","/ZI","/Od","/RTC1","/MDd","../src/main.c","../src/lego_wireless_protocol/*.c","/I","../src/include"]).returncode!=0 or subprocess.run(["cl","/c","/permissive-","/GS","/utf-8","/W3","/ZW","/Zc:wchar_t","/await","/std:c++17","/Gm-","/sdl","/Zc:inline","/fp:precise","/D","_DEBUG","/D","_WINDOWS","/D","_UNICODE","/D","UNICODE","/errorReport:none","/WX","/Zc:twoPhase-","/Gd","/Oi","/FC","/EHsc","/nologo","/diagnostics:column","/ZI","/Od","/RTC1","/MDd","../src/lego_wireless_protocol/*.cpp","/I","../src/include"]).returncode!=0 or subprocess.run(["link","*.obj","/OUT:lego_wireless_protocol.exe","/DYNAMICBASE","kernel32.lib","user32.lib","gdi32.lib","winspool.lib","comdlg32.lib","advapi32.lib","shell32.lib","ole32.lib","oleaut32.lib","uuid.lib","odbc32.lib","odbccp32.lib","/MACHINE:X64","/SUBSYSTEM:CONSOLE","/ERRORREPORT:none","/NOLOGO","/TLBID:1","/WX","/DEBUG","/INCREMENTAL"]).returncode!=0):
+	os.chdir(cd)
+	sys.exit(1)
 os.chdir(cd)
 if ("--run" in sys.argv):
-	subprocess.run(["build/$$$NAM$$$.exe"])
+	subprocess.run(["build/lego_wireless_protocol.exe"])
--- build.sh
+++
@@ -1,3 +0,0 @@
-@echo off
-clear
-python build.py $@
